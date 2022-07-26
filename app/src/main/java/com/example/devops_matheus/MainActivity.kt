2366412d68012d14fecf020df6d2c7cf119c049f
Matheus Oliveira Goncalves <matheus.oliveiragoncalves@student.hogent.be>
2022-07-26 11:08:56 +0200

    [ADD] Android project

---
+++ app/src/main/java/com/example/devops_matheus/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.devops_matheus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.devops_matheus.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/devops_matheus/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.devops_matheus.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
